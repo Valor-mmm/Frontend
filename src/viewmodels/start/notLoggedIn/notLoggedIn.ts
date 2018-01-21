7eac799217fdbc795293cbae8ef13f04a3faf4e4
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-21 18:54:48 +0100

    Form Validation of login.

--- src/viewmodels/start/notLoggedIn/notLoggedIn.ts
+++ src/viewmodels/start/notLoggedIn/notLoggedIn.ts
@@ -1,25 +1,48 @@
-import {inject} from 'aurelia-framework'
+import {inject, NewInstance, LogManager} from 'aurelia-framework'
 import {EventAggregator} from 'aurelia-event-aggregator';
-import {AuthRole, LoginMessage} from "../../../services/authMessages";
+import {ValidationRules, ValidationController, validateTrigger} from "aurelia-validation";
 import {UserService} from "../../../services/svc/userService";
 
+const logger = LogManager.getLogger('NotLoggedIn');
 
-@inject(EventAggregator, UserService)
+@inject(NewInstance.of(ValidationController), EventAggregator, UserService)
 export class NotLoggedIn {
 
-  email: string;
-  password: string;
+  email: string = '';
+  password: string = '';
 
   eventAggregator: EventAggregator;
   userService: UserService;
+  controller: ValidationController;
 
-  constructor(ea: EventAggregator, userService: UserService) {
+  constructor(vc: ValidationController, ea: EventAggregator, userService: UserService) {
+    this.controller = vc;
     this.eventAggregator = ea;
     this.userService = userService;
+
+    this.controller.validateTrigger = validateTrigger.blur;
+    ValidationRules
+      .ensure((v: NotLoggedIn) => v.email).required().email()
+      .ensure((v: NotLoggedIn) => v.password).required().minLength(5)
+      .on(this);
+  }
+
+  async validate() {
+    try {
+      const result = await this.controller.validate({object: this});
+      return result.valid;
+    } catch (err) {
+      logger.error('Error during validation', err);
+      return false;
+    }
   }
 
   login() {
-    this.userService.authenticate(this.email, this.password);
+    this.validate().then(isValid => {
+      if (isValid) {
+        this.userService.authenticate(this.email, this.password);
+      }
+    });
   }
 
 }
