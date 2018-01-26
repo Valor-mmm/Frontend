df5f6c5ef87528910ff1374871c65d273f105b85
Valor-mmm <stefan1.hauser@st.oth-regensburg.de>
2018-01-26 20:22:58 +0100

    Implementing the logout functionality

--- src/viewmodels/logout/logout.ts
+++ src/viewmodels/logout/logout.ts
@@ -1,17 +1,21 @@
 import {inject} from 'aurelia-framework'
 import {EventAggregator} from 'aurelia-event-aggregator';
 import {LoginMessage} from "../../services/authMessages";
+import {UserData} from "../../services/svc/user/userData";
 
-@inject(EventAggregator)
+@inject(EventAggregator, UserData)
 export class Logout {
 
   eventAggregator: EventAggregator;
 
-  constructor(ea: EventAggregator) {
+  constructor(ea: EventAggregator, private userData: UserData) {
     this.eventAggregator = ea;
   }
 
   logout() {
+    this.userData.loggedInUser = null;
+    this.userData.userFriends = null;
+    this.userData.allUsers = null;
     this.eventAggregator.publish(new LoginMessage(null, false));
   }
 
